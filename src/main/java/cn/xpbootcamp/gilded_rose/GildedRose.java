10e66deb87a1800982bad3606c2d09ebc573503e
kaliu <kaliu@thoughtworks.com>
2020-05-19 20:28:21 +0800

    [kaliu] 合并下面的if

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -33,15 +33,16 @@ class GildedRose {
                     }
                 }
             }
+            item.sell_in = item.sell_in - 1;
         } else if (item.name.equals(AGED_BRIE)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
+            item.sell_in = item.sell_in - 1;
         } else if (item.quality > 0 && !item.name.equals(SULFURAS_HAND)) {
             item.quality = item.quality - 1;
-        }
-
-        if (!item.name.equals(SULFURAS_HAND)) {
+            item.sell_in = item.sell_in - 1;
+        } else if (!item.name.equals(SULFURAS_HAND)) {
             item.sell_in = item.sell_in - 1;
         }
 
