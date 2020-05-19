b04929b5beea3ec2677c48083e96b1cea21ce3fa
kaliu <kaliu@thoughtworks.com>
2020-05-19 21:00:20 +0800

    [kaliu] 调整语句顺序

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -48,10 +48,10 @@ class GildedRose {
                 }
             }
         } else if (!item.name.equals(SULFURAS_HAND)) {
-            item.sell_in = item.sell_in - 1;
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
+            item.sell_in = item.sell_in - 1;
             if (item.sell_in < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
