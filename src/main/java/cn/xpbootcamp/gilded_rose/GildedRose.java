6c4e990da11ff7028e8905dc2d9ebc53dc789cba
kaliu <kaliu@thoughtworks.com>
2020-05-19 20:38:04 +0800

    [kaliu] 下面的if提到上面三个if中

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -34,32 +34,65 @@ class GildedRose {
                 }
             }
             item.sell_in = item.sell_in - 1;
+            if (item.sell_in < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS_HAND)) {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    } else {
+                        item.quality = 0;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
         } else if (item.name.equals(AGED_BRIE)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
             item.sell_in = item.sell_in - 1;
+            if (item.sell_in < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS_HAND)) {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    } else {
+                        item.quality = 0;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
         } else if (!item.name.equals(SULFURAS_HAND)) {
             item.sell_in = item.sell_in - 1;
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
-        }
-
-        if (item.sell_in < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS_HAND)) {
-                            item.quality = item.quality - 1;
+            if (item.sell_in < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS_HAND)) {
+                                item.quality = item.quality - 1;
+                            }
                         }
+                    } else {
+                        item.quality = 0;
                     }
                 } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             }
         }
