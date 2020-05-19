6e85616818b935a2117d806bdb22e301fb6dae79
kaliu <kaliu@thoughtworks.com>
2020-05-19 20:49:32 +0800

    [kaliu] 第一个大if中标记为true或false的子if

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -35,10 +35,10 @@ class GildedRose {
             }
             item.sell_in = item.sell_in - 1;
             if (item.sell_in < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                if (true) {
+                    if (false) {
                         if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_HAND)) {
+                            if (!false) {
                                 item.quality = item.quality - 1;
                             }
                         }
