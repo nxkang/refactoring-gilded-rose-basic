9b4ea2d126635319f8bc6cf15756b1187081d180
kaliu <kaliu@thoughtworks.com>
2020-05-19 21:03:00 +0800

    [kaliu] move update_item_quality to item class

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,9 +1,6 @@
 package cn.xpbootcamp.gilded_rose;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS_HAND = "Sulfuras, Hand of Ragnaros";
 
     Item[] items;
 
@@ -13,50 +10,8 @@ class GildedRose {
 
     public void update_quality() {
         for (int i = 0; i < items.length; i++) {
-            update_item_quality(items[i]);
+            items[i].update_item_quality();
         }
     }
 
-    private void update_item_quality(Item item) {
-        if (item.name.equals(BACKSTAGE_PASSES)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-                if (item.sell_in < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sell_in < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
-            item.sell_in = item.sell_in - 1;
-            if (item.sell_in < 0) {
-                item.quality = 0;
-            }
-        } else if (item.name.equals(AGED_BRIE)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-            item.sell_in = item.sell_in - 1;
-            if (item.sell_in < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        } else if (!item.name.equals(SULFURAS_HAND)) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-            item.sell_in = item.sell_in - 1;
-            if (item.sell_in < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        }
-    }
 }
--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -2,6 +2,10 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Item {
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND = "Sulfuras, Hand of Ragnaros";
+
     public String name;
 
     public int sell_in;
@@ -17,5 +21,48 @@ public class Item {
     @Override
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
+    }
+
+    void update_item_quality() {
+        if (name.equals(BACKSTAGE_PASSES)) {
+            if (quality < 50) {
+                quality = quality + 1;
+                if (sell_in < 11) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+
+                if (sell_in < 6) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+            }
+            sell_in = sell_in - 1;
+            if (sell_in < 0) {
+                quality = 0;
+            }
+        } else if (name.equals(AGED_BRIE)) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+            sell_in = sell_in - 1;
+            if (sell_in < 0) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        } else if (!name.equals(SULFURAS_HAND)) {
+            if (quality > 0) {
+                quality = quality - 1;
+            }
+            sell_in = sell_in - 1;
+            if (sell_in < 0) {
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+            }
+        }
     }
 }
