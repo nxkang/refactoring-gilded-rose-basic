c9bfa1c8f24b8c848612ee221710c1f66f9b8464
kaliu <kaliu@thoughtworks.com>
2020-05-19 20:30:03 +0800

    [kaliu] 合并两个else if

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -39,11 +39,11 @@ class GildedRose {
                 item.quality = item.quality + 1;
             }
             item.sell_in = item.sell_in - 1;
-        } else if (item.quality > 0 && !item.name.equals(SULFURAS_HAND)) {
-            item.quality = item.quality - 1;
-            item.sell_in = item.sell_in - 1;
         } else if (!item.name.equals(SULFURAS_HAND)) {
             item.sell_in = item.sell_in - 1;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
         }
 
         if (item.sell_in < 0) {
